184e482ca21be06160dc03d90b04d4543a5be017
franck gaspoz <franck.gaspoz@gmail.com>
2022-12-08 19:29:19 +0100

    project init

---
+++ FileDuplicateAnalyzer/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace FileDuplicateAnalyzer;
+
+/// <summary>
+/// main
+/// </summary>
+public class Program
+{
+    const int ExitOk = 0;
+
+    /// <summary>
+    /// command line input
+    /// </summary>
+    /// <param name="args">arguments</param>
+    /// <returns>status code</returns>
+    public static int Main(string[] args)
+    {
+        return ExitOk;
+    }
+}
